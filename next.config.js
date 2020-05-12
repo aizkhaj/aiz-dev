fd6bfa8cc71820d0af8dfd0b777b5043b4ed069f
Aizaz Khaja <aizaz@halalscope.co>
2020-05-12 17:55:54 -0400

    configured next-mdx-enhanced and installed plugins for it

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
-const withMDX = require('@next/mdx')()
+const readingTime = require('reading-time');
+const withMdxEnhanced = require('next-mdx-enhanced');
+const mdxPrism = require('mdx-prism');
 
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'md', 'mdx']
-});
+
+module.exports = withMdxEnhanced({
+  layoutPath: 'layouts',
+  defaultLayout: true,
+  remarkPlugins: [
+    require('remark-slug'),
+    require('remark-autolink-headings'),
+    require('remark-code-titles'),
+  ],
+  rehypePlugins: [mdxPrism],
+  extendFrontMatter: {
+    process: (mdxContent) => ({
+      readingTime: readingTime(mdxContent)
+    })
+  }
+})()
