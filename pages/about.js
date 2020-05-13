83c7c6261a6a536563dced448537ae78e271a4ce
Aizaz Khaja <aizaz@halalscope.co>
2020-05-13 03:02:16 -0400

    committing missed files

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Container from "../components/Container";
+import Stack from "../components/Stack";
+import Heading from "../components/Heading";
+
+const About = () => (
+  <Container>
+    <Stack>
+      <div className="mt-7 mb-10">
+        <Heading>About</Heading>
+      </div>
+      <p>This page is under progress though, so bear with me 🤓</p>
+    </Stack>
+  </Container>
+);
+
+export default About;
---
+++ pages/blog.js
@@ -0,0 +1,16 @@
+import Container from "../components/Container";
+import Stack from "../components/Stack";
+import Heading from "../components/Heading";
+
+const Blog = () => (
+  <Container>
+    <Stack>
+      <div className="mt-7 mb-10">
+        <Heading>Blog</Heading>
+      </div>
+      <p>This page is under progress though, so bear with me 🤓</p>
+    </Stack>
+  </Container>
+);
+
+export default Blog;
